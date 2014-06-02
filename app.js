5bdde494fa1e239b61ef4b3a1826b3d51046949c
cmy2k <jmhconsultingllc@gmail.com>
2014-06-02 09:03:35 -0400

    Some outlining

--- app.js
+++ app.js
@@ -7,11 +7,46 @@ var fs = require( 'fs' );
 // command-line interface
 cli
     .version( '0.0.1' )
-    .option( '-c, --credentials <path>', 'path to credentials file relative to app.js; defaults to ./awskeys.json' )
+    .option( '-c, --config <path>', 'path to config file relative to app.js; defaults to ./config.json' )
     .parse( process.argv );
 
 // load from command-line arg, failover to awskeys.json; will always look relative to script location
-var credentials = require( cli.credentials ? __dirname + '/' + cli.credentials : __dirname + '/awskeys.json' );
+var config = require( cli.config ? __dirname + '/' + cli.config : __dirname + '/config.json' );
+aws.config.update( config.awsConfig );
+
+// tools
+/*
+  // cloud
+  --cloud-list
+  --cloud-compare <path> path to existing compare, will compare with provided config
+  --cloud-deploy
+  --cloud-snapshot <path> path to store config
+
+  // ec2
+  --ec2-list
+  --ec2-stop
+  --ec2-start
+  --ec2-set-instance
+  --ec2-attach-ebs
+  --ec2-attach-elasticip
+  --ec2-set-sg
+
+  // s3
+  --s3-list-buckets
+  --s3-create-bucket
+  --s3-push-to-bucket
+  --s3-du-bucket
+
+  // security groups
+  --sg-list
+
+  // elasticip
+  --elasticip-list
+  --elasticip-release
+  --elasticip-create
+
+
+*/
 
 /* alternate async file load
 fs.readFile( __dirname + pathToCredentials, 'utf8', function ( err, data ) {
@@ -20,4 +55,4 @@ fs.readFile( __dirname + pathToCredentials, 'utf8', function ( err, data ) {
 });
 */
 
-console.log( credentials.awskeys.region );
+console.log( config.awskeys.region );
